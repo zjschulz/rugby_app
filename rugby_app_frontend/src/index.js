8a4f5d48935a58a8c7ce160aa3df0cf1fc2c92f4
zjschulz <zjschulz@gmail.com>
2020-06-30 20:33:34 +0000

    included redux inindex.js and created basic reducer templates

--- rugby_app_frontend/src/index.js
+++ rugby_app_frontend/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style/index.css';
 import App from './components/App';
+import './style/index.css';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/index'
+
+let store = createStore(rootReducer)
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
---
+++ rugby_app_frontend/src/reducers/quotes.js
@@ -0,0 +1,14 @@
+export default (state = [], action) => {
+    switch (action.type) {
+      case "A":
+        return state
+      case "B":
+        return state
+      case "C":
+        return state
+      case "D":
+        return state
+      default:
+        return state;
+    }
+  }
