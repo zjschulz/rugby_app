18533fc7189c677d02c19256408b79642c6617b9
zjschulz <zjschulz@gmail.com>
2020-06-30 19:03:54 +0000

    set up sessions controller and current user concern

--- rugby_app_backend/app/controllers/application_controller.rb
+++ rugby_app_backend/app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+    include ActionController::Cookies
+    include ActionController::RequestForgeryProtection
+    protect_from_forgery with: :exception
+    before_action :set_csrf_cookie
+    skip_before_action :verify_authenticity_token
+
+
+    private
+
+    def set_csrf_cookie
+        cookies["CSRF-TOKEN"] = form_authenticity_token
+    end
 end
---
+++ rugby_app_backend/app/controllers/sessions_controller.rb
@@ -0,0 +1,37 @@
+class SessionsController < ApplicationController
+  include CurrentUserConcern
+
+    def create
+      user = User
+              .find_by(email: params[:email])
+              .try(:authenticate, params[:password])
+      if user
+        session[:user_id] = user.id
+        render json: {
+          status: :created,
+          logged_in: true,
+          user: user
+        }
+      else
+        render json: { 
+          status: 401,
+          logged_in: false
+        }
+      end
+    end
+    
+    def logged_in
+      if @current_user
+      else
+        #render json: {
+        #  logged_in: false
+        #}
+      end
+    end
+
+    def logout
+      reset_session
+      render json: { status: 200, logged_out: true }
+    end
+
+end
