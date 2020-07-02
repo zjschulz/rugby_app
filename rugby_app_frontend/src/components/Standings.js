77ad6d6656a2639575cf9f3bba46219d909573fd
zjschulz <zjschulz@gmail.com>
2020-07-02 19:50:11 +0000

    made minor format edits

--- rugby_app_frontend/src/components/Standings.js
+++ rugby_app_frontend/src/components/Standings.js
@@ -19,7 +19,7 @@ export default class Standings extends React.Component {
         }))
         .catch(err => alert(err));
     };
-    // .filter(x => x.user_id == this.props.user.id).
+
     generateTeams = () => {
         return this.state.data.filter(x => x.user_id == this.props.user.id).map(team => <Team
           name = {team.name}
--- rugby_app_frontend/src/components/Team.js
+++ rugby_app_frontend/src/components/Team.js
@@ -1,19 +1,9 @@
 import React, { Component } from 'react';
 
-
 export default class Team extends Component {
     render(){
         return(
             <tr>
-                {/* <td>North Penn Rugby</td> 
-                <td>2</td>
-                <td>4</td>
-                <td>0</td> 
-                <td>202</td>
-                <td>183</td>
-                <td>19</td> 
-                <td>3</td>
-                <td>11</td> */}
                 <td>{this.props.name}</td> 
                 <td>{this.props.wins}</td>
                 <td>{this.props.losses}</td>
