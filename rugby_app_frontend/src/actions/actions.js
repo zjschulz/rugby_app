c7366e87914aa1501c9cf7e7019ffa2b3c10e937
zjschulz <zjschulz@gmail.com>
2020-07-06 15:15:07 +0000

    added ADD_TEAM functionality to actions and reducers

--- rugby_app_frontend/src/actions/actions.js
+++ rugby_app_frontend/src/actions/actions.js
@@ -1,11 +1,21 @@
 import fetch from 'isomorphic-fetch';
 
 export function fetchTeams() {
-  return (dispatch) => {
-    dispatch({ type: 'GET_TEAMS' });
-    return fetch('http://localhost:3001/teams')
-      .then(response => response.json())
-      .then(data => dispatch({ type: 'GET_TEAMS', payload: data }))
-      .catch(err => alert(err))
-  };
+    return (dispatch) => {
+        dispatch({ type: 'GET_TEAMS' });
+        return fetch('http://localhost:3001/teams')
+        .then(response => response.json())
+        .then(data => dispatch({ type: 'GET_TEAMS', payload: data }))
+        .catch(err => alert(err))
+    };
+}
+
+export function addTeam() {
+    return (dispatch) => {
+        dispatch({ type: 'ADD_TEAM'});
+        return fetch('http://localhost:3001/teams')
+        .then(response => response.json())
+        .then(data => dispatch({ type: 'ADD_TEAM', payload: data }))
+        .catch(err => alert(err)) 
+    };
 }
